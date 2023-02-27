3ad9bfa909edbbf1e8c985e3e2a366cd4dd22870
Júlio Mendonça <juliommen@gmail.com>
2023-02-27 14:08:47 -0400

    final commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@storage': './src/storage',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+            '@theme': './src/theme',
+          },
+        },
+      ],
+    ],
+  }
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { useNavigation } from "@react-navigation/native";
+
+import { BackButton, BackIcon, Container, Logo } from "./styles";
+
+import logoImg from '@assets/logo.png';
+
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
+
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+
+  return (
+    <Container>
+      { 
+        showBackButton &&
+        <BackButton onPress={handleGoBack}>
+          <BackIcon name="caret-left" />
+        </BackButton>
+      }
+
+      <Logo source={logoImg} />
+    </Container>
+  )
+}
